acb2f1894d5666e8b74997e00fd91eef6e645983
Michael Dietz <michael.dtz@gmail.com>
2018-02-09 15:26:02 +0100

    Update to the lib and readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # whlayout
-WarehouseLayout
+WarehouseLayout Generator
+
+This lib allows you to generate a warehouse layout based on a given polygon and with layout parameters
+
+## Getting Started
+´´´ import "github.com/md-golibs/whlayout"
+
+In order to call the function GenerateLayout you need to pass the a []byte with the GeoJSON containing a polygon and the layout definition (type whlayout.LayoutParameters).
+
+Remark: The polygon should only consist of four points and the order of the points should be: lower left corner, lower right corner, upper right corner, upper left corner. This is currently still a limitation (sorry).
+
+´´´
+    //Create the LayoutParamters
+	layout := wly.LayoutParameters{ }
+	layout.Horizontal = false
+	layout.BinsPerRack = 10
+	layout.HLayout = []float64{1.075,0.05,1.05,0.05,1.05,0.05,1.05,0.05,1.05,0.05,1.05,0.05,1.05,0.05,1.05,0.05,1.05,0.05,1.075}
+	layout.VLayout = []float64{1.1,0.35,1.1,0.35,1.1}
+	layout.HDocks  = []float64{0.05,1.9,0.05}
+	layout.VDocks  = []float64{1.1,0.2,1.1,0.2,1.1,0.2,1.1}
+	
+	var wh wly.Warehouse = wly.GenerateLayout(polygonGeoJSON, layout)
+´´´
--- whlayout.go
+++ whlayout.go
@@ -1,73 +1,79 @@
 package wly
 
-import "github.com/twpayne/go-geom"
+import (
+	"github.com/twpayne/go-geom"
+	"github.com/twpayne/go-geom/encoding/geojson"
+)
 
-
-type LayoutParameters struct{
-	Horizontal bool
-	HLayout []float64
-	VLayout []float64
-	HDocks []float64
-	VDocks []float64
-	RackSize float64
-	AisleSize float64
-	Padding float64
+type LayoutParameters struct {
+	Horizontal  bool
+	HLayout     []float64
+	VLayout     []float64
+	HDocks      []float64
+	VDocks      []float64
+	RackSize    float64
+	AisleSize   float64
+	Padding     float64
 	BinsPerRack int
 }
 
-type Rack struct{
-	Id string
-	Outline geom.Polygon
-	AsGeoJSON string
-	StorageBins []StorageBin
+type Rack struct {
+	Id            string
+	Outline       geom.Polygon
+	AsGeoJSON     string
+	StorageBins   []StorageBin
 	BinsAsGeoJSON string
 }
 
-type StorageBin struct{
-	Id string
-	Outline geom.Polygon
+type StorageBin struct {
+	Id        string
+	Outline   geom.Polygon
 	AsGeoJSON string
-	Center geom.Point
+	Center    geom.Point
 }
 
-type Dock struct{
-	Id string
-	Outline geom.Polygon
+type Dock struct {
+	Id        string
+	Outline   geom.Polygon
 	AsGeoJSON string
-	Center geom.Point	
+	Center    geom.Point
 }
 
-type Warehouse struct{
-	Outline geom.Polygon
-	AsGeoJSON string
-	Racks []Rack
+type Warehouse struct {
+	Outline        geom.Polygon
+	AsGeoJSON      string
+	Racks          []Rack
 	RacksAsGeoJSON string
-	Docks []Dock
+	Docks          []Dock
 	DocksAsGeoJSON string
-} 
+}
 
-
-func GenerateLayout(polygon *geom.Polygon, layout LayoutParameters) Warehouse {
+func GenerateLayout(polygonGeoJSON []byte, layout LayoutParameters) Warehouse {
 
 	var wh Warehouse
+
+	geometry := new(geom.T)
+	var err = geojson.Unmarshal(polygonGeoJSON, geometry)
+	check(err)
+	polygon := (*geometry).(*geom.Polygon)
+
 	var points []geom.Point = edgePoints(polygon)
 	var padding = meterToDeg(layout.Padding)
-		
+
 	var p1 = addPadding(points[0], padding, 1)
 	var p2 = addPadding(points[1], padding, 2)
 	var p3 = addPadding(points[2], padding, 3)
-	var p4 = addPadding(points[3], padding, 4) 
+	var p4 = addPadding(points[3], padding, 4)
 
 	wh.Racks = []Rack{}
 	wh.Outline = *polygon
-	wh.AsGeoJSON = toPolygonGeoJSON([]geom.Point{p1,p2,p3,p4,p1})
+	wh.AsGeoJSON = toPolygonGeoJSON([]geom.Point{p1, p2, p3, p4, p1})
 
 	wh.Racks, wh.RacksAsGeoJSON = generateRacks(wh, layout)
 	wh.Docks, wh.DocksAsGeoJSON = generateDocks(wh, layout)
 
 	return wh
 }
-
 
 func check(e error) {
 	if e != nil {
